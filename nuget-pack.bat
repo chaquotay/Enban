c861381586e31da1d4e1f698e1cf0048565c4762
Chaquotay <stephan@notatoaster.org>
2020-04-18 15:12:42 +0200

    Removed empty packages.config and switched to nuget pack with explicit nuspec, due to https://github.com/NuGet/Home/issues/8713 (NU5128)

--- nuget-pack.bat
+++ nuget-pack.bat
@@ -1,2 +1,6 @@
+@ECHO OFF
+
 mkdir build
-nuget pack src\Enban\Enban.csproj -exclude "**\Countries\*.*" -outputDirectory build
+REM Has problems due to https://github.com/NuGet/Home/issues/8713 (NU5128):
+REM nuget pack src\Enban\Enban.csproj -exclude "**\Countries\*.*" -outputDirectory build -prop Configuration=Release
+nuget pack src\Enban\Enban.nuspec -outputDirectory build -prop Configuration=Release
