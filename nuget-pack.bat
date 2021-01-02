117cdcec5f702b5d1bd0f7f9bfa1ed0f7a6392c9
Chaquotay <stephan@notatoaster.org>
2021-01-02 21:14:53 +0100

    Migrate project to SDK style
    Fixes #3

--- nuget-pack.bat
+++ nuget-pack.bat
@@ -1,6 +1,4 @@
 @ECHO OFF
 
 mkdir build
-REM Has problems due to https://github.com/NuGet/Home/issues/8713 (NU5128):
-REM nuget pack src\Enban\Enban.csproj -exclude "**\Countries\*.*" -outputDirectory build -prop Configuration=Release
-nuget pack src\Enban\Enban.nuspec -outputDirectory build -prop Configuration=Release
+dotnet pack src\Enban\Enban.csproj --output build --configuration Release
--- src/Enban/Properties/AssemblyInfo.cs
+++ src/Enban/Properties/AssemblyInfo.cs
@@ -1,18 +1,3 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("IBAN")]
-[assembly: AssemblyDescription("Enban is an IBAN/BBAN API for .NET, including parsing, validation and formatting.")]
-[assembly: AssemblyProduct("IBAN")]
-[assembly: AssemblyCopyright("Copyright © 2018 Stephan Müller")]
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyInformationalVersion("0.1.0-alpha")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
-
-[assembly: ComVisible(false)]
-
-[assembly: Guid("0c93e7bf-aa87-4639-89e8-d9c9a715d3c7")]
-
+﻿using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("Enban.Test")]
