78b78bc4c87ed60406ec8b86be307b961f36b4be
Chaquotay <stephan@notatoaster.org>
2021-01-02 22:16:52 +0100

    Dead code removed

--- src/Enban/Text/TextExtensions.cs
+++ src/Enban/Text/TextExtensions.cs
@@ -6,49 +6,20 @@ namespace Enban.Text
     {
         public static char[] RemoveWhitespaces(this char[] text)
         {
-            var whitespaceCount = 0;
-            var inWhitespace = false;
-
-            var src = text;
             var chars = new char[text.Length];
             var offset = 0;
-            var start = 0;
 
-            for (var i = 0; i < text.Length; i++)
+            foreach (var c in text)
             {
-                var c = text[i];
-                if (c == ' ' || c == '\t')
-                {
-                    //whitespaceCount++;
-                    //if (!inWhitespace)
-                    //{
-                    //    var len = i - start;
-                    //    Array.Copy(src, start, chars, offset, len);
-                    //    offset += len;
-                    //}
-                    //inWhitespace = true;
-                }
-                else
+                if (c != ' ' && c != '\t')
                 {
                     chars[offset++] = c;
-                    //if (inWhitespace)
-                    //{
-                    //    start = i;
-                    //    inWhitespace = false;
-                    //}
                 }
             }
 
-            //if (!inWhitespace)
-            //{
-            //    Array.Copy(src, start, chars, offset, src.Length - start);
-            //}
-
-            //var t = new char[chars.Length - whitespaceCount];
-            var t = new char[offset];
-            Array.Copy(chars, 0, t, 0, t.Length);
-            //var t = new string(chars, 0, chars.Length - whitespaceCount);
-            return t;
+            var result = new char[offset];
+            Array.Copy(chars, 0, result, 0, result.Length);
+            return result;
         }
 
     }
